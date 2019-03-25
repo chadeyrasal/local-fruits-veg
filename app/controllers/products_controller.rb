daa1e6630a861026d7a4acda909bc761114335f9
chadeyrasal <annelaure.chadeyras@gmail.com>
2019-03-25 16:29:46 +0000

    can create and edit products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,4 +16,53 @@ class ProductsController < ApplicationController
     end
   end
 
+  get "/products/:id" do
+    if logged_in?
+      @product = Product.find_by(:id => params[:id])
+      erb :"products/show"
+    else
+      redirect "/"
+    end
+  end
+
+  post "/products" do
+    if params[:name] == "" || params[:quantity] == ""
+      redirect "/products/new"
+    else
+      @product = Product.new(:name => params[:name], :quantity => params[:quantity], :unit => params[:unit])
+      @product.save
+      redirect "/products/#{@product.id}"
+    end
+  end
+
+  get "/products/:id/edit" do
+    if logged_in?
+      @product = Product.find_by(:id => params[:id])
+      erb :"products/edit"
+    else
+      redirect "/"
+    end
+  end
+
+  patch "/products/:id" do
+    if logged_in?
+      if params[:name] == "" || params[:quantity] == ""
+        redirect "/product/#{params[:id]}/edit"
+      else
+        @product = Product.find_by(:id => params[:id])
+        if @product.user == current_user
+          @product.name = params[:name]
+          @product.quantity = params[:quantity]
+          @product.unit = params[:unit]
+          @product.save
+          redirect "/products/#{@product.id}"
+        else
+          redirect "/products"
+        end
+      end
+    else
+      redirect "/"
+    end
+  end
+
 end
--- app/models/product_unit.rb
+++
@@ -1,2 +0,0 @@
-class ProductUnit < ActiveRecord::Base
-end
